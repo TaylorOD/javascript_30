2bc13119417923e40aa25af41a783d863a1eda92
taylorod <taylorsunwantedemails@gmail.com>
2020-12-04 10:29:45 -0600

    add play javascript function

--- 11_custom_video/scripts.js
+++ 11_custom_video/scripts.js
@@ -5,12 +5,18 @@ const video = player.querySelector(".viewer")
 const progress = player.querySelector(".progress")
 const progressBar = player.querySelector(".progress__filled")
 const toggle = player.querySelector(".toggle")
-const skipButtons = player.querySelector(".[data-skip]")
+const skipButtons = player.querySelector("[data-skip]")
 const ranges = player.querySelector(".player__slider")
 
 
 // Build functions
-
+function togglePlay() {
+  if (video.paused) {
+    video.play()
+  } else {
+    video.pause()
+  }
+}
 
 
 // Hook up event listerns 
