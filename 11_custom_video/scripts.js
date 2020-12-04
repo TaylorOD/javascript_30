6c399ffa1d1222c5639abd8e482c52a122e5fbf9
taylorod <taylorsunwantedemails@gmail.com>
2020-12-04 10:41:07 -0600

    add all to multiple query selectors

--- 11_custom_video/scripts.js
+++ 11_custom_video/scripts.js
@@ -5,8 +5,8 @@ const video = player.querySelector(".viewer")
 const progress = player.querySelector(".progress")
 const progressBar = player.querySelector(".progress__filled")
 const toggle = player.querySelector(".toggle")
-const skipButtons = player.querySelector("[data-skip]")
-const ranges = player.querySelector(".player__slider")
+const skipButtons = player.querySelectorAll("[data-skip]")
+const ranges = player.querySelectorAll(".player__slider")
 
 
 // Build functions
@@ -20,8 +20,13 @@ function togglePlay() {
 
 function updateButton() {
   const icon = this.paused ? "►" : "❚ ❚"
-  toggle.textContent = icon
+  toggle.textContent = icon 
+}
+
+function skip() {
+  console.log(this.dataset.skip)
   
+
 }
 
 // Hook up event listeners 
@@ -29,4 +34,6 @@ function updateButton() {
 video.addEventListener("click", togglePlay)
 video.addEventListener("play", updateButton)
 video.addEventListener("pause", updateButton)
+
 toggle.addEventListener("click", togglePlay)
+skipButtons.forEach(button => button.addEventListener("click", skip))
